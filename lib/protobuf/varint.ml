1c1a6d62220d655a4e408371b80821dd333f471b
orbitz <orbitz@gmail.com>
2013-02-10 23:20:45 +0100

    Add initial wire protocol parse support

--- lib/protobuf/varint.ml
+++ lib/protobuf/varint.ml
@@ -11,15 +11,15 @@ let i64       = Int64.of_int
 let rec read_byte n acc bits =
   bitmatch bits with
     | { true  : 1
-      ; b0    : 7
+      ; b     : 7
       ; rest  : -1 : bitstring
       } ->
-      read_byte (n + 1) (acc +^ (i64 b0 <<< (7 * n))) rest
+      read_byte (n + 1) (acc +^ (i64 b <<< (7 * n))) rest
     | { false : 1
-      ; b0    : 7
+      ; b     : 7
       ; rest  : -1 : bitstring
       } ->
-      Ok (acc +^ (i64 b0 <<< (7 * n)), rest)
+      Ok (acc +^ (i64 b <<< (7 * n)), rest)
     | { _ } ->
       Error `Incomplete
 
