342fc2094467847e18b9f4c09aacba080322a1a8
orbitz <orbitz@gmail.com>
2013-03-10 21:50:39 +0100

    Add completely untested fixed integer to_string functions

--- lib/protobuf/fixed32.ml
+++ lib/protobuf/fixed32.ml
@@ -16,4 +16,15 @@ let of_bitstring bits =
     | { _ } ->
       Error `Incomplete
 
-let to_string t = failwith "nyi"
+let shift_and_char t n =
+  let open Int32 in
+  Char.of_int_exn (to_int_exn (shift_right t n) land 0xff)
+
+let to_string t =
+  let s = String.create 4 in
+  s.[3] <- shift_and_char t 24;
+  s.[2] <- shift_and_char t 16;
+  s.[1] <- shift_and_char t 8;
+  s.[0] <- Char.of_int_exn ((Int32.to_int_exn t) land 0xff);
+  s
+
